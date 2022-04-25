d078fb84ab456cc060058b19f8f00b54552cc019
Egon Meijers <egon.meijers@signify.com>
2022-04-25 20:54:58 +0200

    Add code documentation for SecureStorageService

---
+++ lib/core/services/api_client.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_client.g.dart';
+
+@RestApi(baseUrl: "https://meijers-hassio.duckdns.org/api")
+abstract class ApiClient {
+  factory ApiClient(Dio dio, {String baseUrl}) = _RestClient;
+
+  @GET("/tasks")
+  Future<List<Task>> getTasks();
+}
+
+@JsonSerializable()
+class Task {
+  String? id;
+  String? name;
+  String? avatar;
+  String? createdAt;
+
+  Task({this.id, this.name, this.avatar, this.createdAt});
+
+  factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+  Map<String, dynamic> toJson() => _$TaskToJson(this);
+}
--- lib/core/services/secure_storage_service.dart
+++ lib/core/services/secure_storage_service.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+/// {@template SecureStorageService}
+/// Service class used as a bridge to communicate with [FlutterSecureStorage].
+/// {@endtemplate}
 class SecureStorageService {
+  /// {@macro SecureStorageService}
   factory SecureStorageService({FlutterSecureStorage? flutterSecureStorage}) {
     _instance = SecureStorageService._internal(
       flutterSecureStorage ?? const FlutterSecureStorage(),
@@ -9,18 +13,23 @@ class SecureStorageService {
     return _instance;
   }
 
+  /// {@macro SecureStorageService}
   SecureStorageService._internal(this._flutterSecureStorage);
 
   static SecureStorageService get instance => _instance;
 
+  /// {@macro SecureStorageService}
   static late SecureStorageService _instance;
   late final FlutterSecureStorage _flutterSecureStorage;
 
+  /// Identifier to write and read the access token value
   static const accessTokenKey = 'access_token';
 
+  /// Retrieves the value of [accessTokenKey] stored in [FlutterSecureStorage]
   Future<String?> readAccessToken() async =>
       await _flutterSecureStorage.read(key: accessTokenKey);
 
+  /// Saves the [accessToken] to [FlutterSecureStorage] using [accessTokenKey]
   Future<void> writeAccessToken(String accessToken) async {
     await _flutterSecureStorage.write(key: accessTokenKey, value: accessToken);
   }
